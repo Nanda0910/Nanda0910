0c030732458fae209e0a9f0f63e82017c0d7ca93
Nanda0910 <82806988+Nanda0910@users.noreply.github.com>
2021-12-13 14:45:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup() {
-  canvas = createCanvas(640, 480);
-  canvas.position(110, 250);
+  canvas = createCanvas(300, 300);
+  canvas.center();
   video = createCapture(VIDEO);
+  video.size(300, 300);
   video.hide();
 
-  tint_color = "";
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+  }
 }
 
 function draw() {
-  image(video, 0, 0, 640, 480);
-  tint(tint_color);
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){    
-  save('student_name.png');
+  save('myFilterImage.png');
 }
-
-function filter_tint()
-{
-	tint_color = document.getElementById("color_name").value;
-}
-
